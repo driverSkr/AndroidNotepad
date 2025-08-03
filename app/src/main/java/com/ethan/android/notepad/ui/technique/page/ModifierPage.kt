9a7394771b9a55fbbbd62a3afa5987bd22a39568
driverSkr <2237913536@qq.com>
2025-08-04 01:07:31 +0800

    添加modifier页

---
+++ app/src/main/java/com/ethan/android/notepad/ui/technique/page/ModifierPage.kt
@@ -0,0 +1,34 @@
+package com.ethan.android.notepad.ui.technique.page
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.ethan.android.notepad.ui.custom.model.CardItem
+import com.ethan.android.notepad.ui.custom.view.ListCardView
+import com.ethan.android.notepad.ui.custom.view.StatusBarsView
+import com.ethan.android.notepad.ui.technique.TechniquePreviewActivity
+import com.ethan.android.notepad.ui.technique.model.PageType
+
+/**
+ * Modifier.onGloballyPositioned {  }
+ * Modifier.onPlaced {  }
+ */
+@Composable
+@Preview
+fun ModifierPage() {
+    val context = LocalContext.current
+    val items = listOf(
+        CardItem("Pair和Triple的使用", true, isCompleted = false),
+        CardItem("aspectRatio设置宽高比", true, isCompleted = false),
+        CardItem("imePadding自动增加底部内边距", true) { TechniquePreviewActivity.launch(context, PageType.ImePadding) },
+    )
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        StatusBarsView("Modifier实用方法")
+
+        ListCardView(items = items)
+    }
+}
